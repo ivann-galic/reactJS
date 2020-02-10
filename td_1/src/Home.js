15827975e156616a38844ac54a6e9be873294c34
Ivann <ivann.galic@edu.itescia.fr>
2020-02-10 15:01:22 +0100

    TD 1 premère partie

---
+++ td_1/src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from "./logo.svg";
+
+function Apropos() {
+    return (
+        <div className="A propos">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Hello !
+                </p>
+
+            </header>
+        </div>
+    );
+}
