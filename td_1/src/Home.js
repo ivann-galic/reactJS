21ffa48c527496d425486f4c71748b560b674cfd
Ivann <ivann.galic@edu.itescia.fr>
2020-02-11 10:38:28 +0100

    TD1 exemple workshop utilisé

--- td_1/src/Home.js
+++ td_1/src/Home.js
@@ -2,11 +2,23 @@ import React from "react";
 
 export default class Home extends React.Component
 {
+    constructor({fullName}){
+        super();
+        this.state = {
+            fullName: fullName
+        }
+    }
+
+    componentDidUpdate({fullName}) {
+        this.setState({...this.state, fullName});
+    }
+
     render() {
+        const{fullName} = this.state;
         return (
             <div>
-                <h2>Home</h2>
-                <p><getName/>Hello {this.state}!</p>
+                <h2>Acceuil</h2>
+                <p>Hello {fullName} !</p>
             </div>
         );
     }
