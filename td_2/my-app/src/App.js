4a026152af958551241dffd7fdd84beda9cdf070
Ivann <ivann.galic@edu.itescia.fr>
2020-02-11 15:40:47 +0100

    css ok

---
+++ td_2/my-app/src/App.js
@@ -0,0 +1,37 @@
+import React, {Component} from "react";
+import './App.css';
+
+export default class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            myNumber: null
+        }
+    }
+
+    handleInputChange = (event) => {
+        this.props.myNumber(event.target.value);
+    };
+
+    render() {
+        const{myNumber} = this.state;
+        return (
+            <div class="container">
+                <div class="app">
+                    <form>
+                        <label>Entrez un nombre entre 0 et 100 :</label>
+                        <input type="number" name="myNumber" onChange={this.handleInputChange}/>
+                        <button id="submit-btn" type="submit">Envoyer</button>
+                        <button >Recommencer</button>
+                    </form>
+                    <div class="reponse">
+                        <p>{myNumber}</p>
+                    </div>
+                </div>
+            </div>
+        );
+
+    }
+}
+
