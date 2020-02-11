7f6f84a88417c8ecd994ece4aefcf3b3873e1b47
Ivann <ivann.galic@edu.itescia.fr>
2020-02-11 09:43:33 +0100

    class Home

--- td_1/src/App.js
+++ td_1/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import About from './About';
 import Config from './Config';
+import Home from './Home';
 
 export default class App extends Component {
 
@@ -12,19 +13,17 @@ export default class App extends Component {
             <nav>
                 <div>
                   <ul>
-                      <li><Link to="/">Accueil</Link></li>
+                      <li><Link to="/Home">Accueil</Link></li>
                       <li><Link to="/Config">Config</Link></li>
                       <li><Link to="/About">À propos</Link></li>
                   </ul>
                 </div>
             </nav>
             <Switch>
+
                 <Route path="/About"><About /></Route>
                 <Route path="/Config"><Config /></Route>
-                <div>
-                    <h2>Home</h2>
-                    <p><getName/>Hello !</p>
-                </div>
+                <Route path="/Home"><Home /></Route>
             </Switch>
         </Router>
     );
---
+++ td_1/src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Home extends React.Component
+{
+    render() {
+        return (
+            <div>
+                <h2>Home</h2>
+                <p><getName/>Hello {this.state}!</p>
+            </div>
+        );
+    }
+}
