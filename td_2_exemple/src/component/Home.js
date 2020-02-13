e17314aa62cb5e6a1a7f140a7554d54bacdebf3b
Ivann <ivann.galic@edu.itescia.fr>
2020-02-13 09:21:55 +0100

    test nombre à trouver

--- td_2_exemple/src/component/Home.js
+++ td_2_exemple/src/component/Home.js
@@ -6,10 +6,25 @@ export default class Home extends React.Component {
 
 	constructor(props) {
 		super(props);
+		this.state = {
+			numberToFind: null,
+			entry: null
+		}
 	}
+
+	change = e =>{
+		this.setState({
+			[e.target.entry]: e.target.value
+		})
+	};
+
+	onSubmit = (e) => {
+		console.log(this.state);
+	};
 
 	render() {
 		var nbToFind = randomNb(101);
+		var entry = null;
 
 		return (
 
@@ -18,12 +33,12 @@ export default class Home extends React.Component {
 					<div class="txt">
 						<form>
 							<label>Entrez un nombre entre 0 et 100 :</label>
-							<input type="number" name="number"/>
-							<button id="submit-btn" type="submit" >Envoyer</button>
+							<input type="number" value={this.state.entry} onChange={e => this.change(e)}/>
+							<button id="submit-btn" type="submit" onClick={this.onSubmit()} >Envoyer</button>
 							<button id="restart-btn">Recommencer</button>
 						</form>
 						<div className="reponse">
-							<p>Le nombre à trouver est : {nbToFind}</p>
+							<p>A trouver : {nbToFind}, rentré :  </p>
 						</div>
 					</div>
 				</div>
--- td_2_exemple/src/component/Number.js
+++ td_2_exemple/src/component/Number.js
@@ -1,8 +1,34 @@
+//let entry = document.getElementById('data').value;
+let count = 0;
+let win = false;
 
 export function randomNb (max) {
     return (Math.floor(Math.random() * max));
 }
 
+/*export function play (myRandom, infos) {
 
+    count++;
+
+    if (myRandom !== entry) {
+        if (myRandom < entry){
+            infos = "C'est plus petit !";
+        }else if (myRandom > entry){
+            infos = "C'est plus grand !";
+        }
+    }
+
+    if(myRandom === entry){
+        infos = "Gagné !";
+/!*        console.log(myRandom);
+        if (myTable.length === 5) {
+            myTable.shift();
+        }
+        myTable.push([myRandom,counter]);
+        displayTable();*!/
+        win = true;
+    }
+
+}*/
 
 
